699f225ae19d619256c7fbc792b884e8ef9a3e66
unknown <ghassan98ar@gmail.com>
2020-03-20 22:23:13 -0400

    Implemented solve() & valid()

--- soduku.py
+++ soduku.py
@@ -16,6 +16,49 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+# solve the board using recursion 
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+            
+            bo[row][col] = 0
+
+    return False
+
+# check if sudoku rules are applied
+def valid(bo, num, pos):
+    # check row
+    for i in range (len(bo[0])):
+        # 
+        if bo[pos[0]][i] == num and pos[1] != i:    # bo[row][col]
+            return False
+
+    # check columns
+    for i in range (len(bo[0])):
+        # 
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # check box
+    box_x = pos[1] // 3     # returns 0, 1, or 2 representing the horizontal box
+    box_y = pos[0] // 3     # returns 0, 1, or 2 representing the vertical box
+
+    for i in range (box_y*3, box_x*3 + 3):
+        for j in range (box_x*3, box_y*3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
+
 # print sudoku board
 def print_board(bo):
     for i in range (len(bo)):   # len(bo) returns the number of elements in array
@@ -41,8 +84,13 @@ def print_board(bo):
 # find empty squares in board
 def find_empty(bo):
     for i in range (len(bo)):
-        for j in range (len(bo[0]))
+        for j in range (len(bo[0])):
             if bo[i][j] == 0:
                 return (i, j)   # return indecies (row, column)
                 
     return None
+
+print_board(board)
+solve(board)
+print ("\n*************************\n*************************\n")
+print_board(board)
